a79723af1ebe98baaa2b21b99031bae8e23ee9a7
ism46999937 <abailonescobero@gmail.com>
2020-10-02 10:17:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>index PHP</title>
+    </head>
+    <body>
+        <h2>
+            <?php
+            //comentario una linea
+            /* comentario
+            varias 
+            lineas */
+            #comentario una linea tambien
+            echo 'Hola mundo';
+            ?>
+        </h2>
+    </body>
+</html>
